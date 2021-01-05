6be9bd723b3f97fb5e28c701e9bd09c8e0e3a9ef
VIGNESHWAR258 <36541704+VIGNESHWAR258@users.noreply.github.com>
2021-01-05 17:48:45 +0530

    github_user_finder

---
+++ firebase-github-app/src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBOGfoTr-2SOragyzPBvGWR7Hj5gaOJS90",
+    authDomain: "mygitapp-af8d1.firebaseapp.com",
+    projectId: "mygitapp-af8d1",
+    storageBucket: "mygitapp-af8d1.appspot.com",
+    messagingSenderId: "409026763960",
+    appId: "1:409026763960:web:8bc05873b8bd76c329684d",
+    measurementId: "G-HG48NN80PL"
+};
